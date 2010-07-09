2f06808aaf1f87473b60a19eabb551ffe9216927
unknown <Owner@.(none)>
2010-07-09 18:57:25 +0900

    commit L01

--- L01.scm
+++ L01.scm
@@ -1,3 +1,74 @@
 ;; L-99
 ;; http://github.com/valvallow/L-99
-;; L01 
+
+;; P01 (*) Find the last box of a list.
+;; Example:
+;; * (my-last '(a b c d))
+;; (D)
+
+
+;; ver 1
+(define (my-last ls)
+  (cond ((null? ls) ls)
+        ((null? (cdr ls)) ls)
+        (else (my-last (cdr ls)))))
+
+(my-last '(a b c d))
+;; (d)
+(my-last '())
+;; ()
+
+;; ver 2
+(define (my-last ls)
+  (if (or (null? ls)
+          (null? (cdr ls)))
+      ls
+      (my-last (cdr ls))))
+
+(my-last '(a b c d))
+;; (d)
+(my-last '())
+;; ()
+
+
+;; gauche
+(last-pair '(a b c d))
+;; (d)
+(last-pair '())
+;; *** ERROR: pair required: ()
+
+
+;; ver 3
+(define (my-last ls)
+  (if (null? (cdr ls))
+      ls
+      (my-last (cdr ls))))
+
+(my-last '(a b c d))
+;; (d)
+(my-last '())
+;; error
+(my-last '(a . b))
+;; error
+
+
+;; gauche
+(last-pair '(a . b))
+;; (a . b)
+
+
+;; ver 4
+(define (my-last ls)
+  (cond ((null? (cdr ls)) ls)
+        ((not (list? (cdr ls))) ls)
+        (else (my-last (cdr ls)))))
+
+(my-last '(a b c d))
+;; (d)
+(my-last '())
+;; error
+(my-last '(a . b))
+;; (a . b)
+
+
+;; answer
