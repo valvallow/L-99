cbef3d97522dc13527d71b3d19e6de8c665e6e60
valvallow <valvalloooooooooow@gmail.com>
2013-06-28 21:33:25 +0900

    update L01

--- L01.scm
+++ L01.scm
@@ -1,166 +1,23 @@
-;; L-99
-;; http://github.com/valvallow/L-99
-
 ;; P01 (*) Find the last box of a list.
 ;; Example:
 ;; * (my-last '(a b c d))
 ;; (D)
 
 
-;; ver 1
-(define (my-last ls)
-  (cond ((null? ls) ls)
-        ((null? (cdr ls)) ls)
-        (else (my-last (cdr ls)))))
-
-(my-last '(a b c d))
-;; (d)
-(my-last '())
-;; ()
-
-;; ver 2
-(define (my-last ls)
-  (if (or (null? ls)
-          (null? (cdr ls)))
-      ls
-      (my-last (cdr ls))))
-
-(my-last '(a b c d))
-;; (d)
-(my-last '())
-;; ()
-
-
-;; gauche
+;; srfi-1
 (last-pair '(a b c d))
-;; (d)
-(last-pair '())
-;; *** ERROR: pair required: ()
 
-
-;; ver 3
+;; my-last
 (define (my-last ls)
-  (if (null? (cdr ls))
-      ls
-      (my-last (cdr ls))))
-
-(my-last '(a b c d))
-;; (d)
-(my-last '())
-;; error
-(my-last '(a . b))
-;; error
+  (let rec ((ls ls))
+    (if (or (null? ls)
+            (null? (cdr ls)))
+        ls
+        (rec (cdr ls)))))
 
 
-;; gauche
-(last-pair '(a . b))
-;; (a . b)
-
-
-;; ver 4
-(define (my-last ls)
-  (cond ((null? (cdr ls)) ls)
-        ((not (list? (cdr ls))) ls)
-        (else (my-last (cdr ls)))))
-
-(my-last '(a b c d))
-;; (d)
-(my-last '())
-;; error
-(my-last '(a . b))
-;; (a . b)
-
-
-;; ver 5
-(define (my-last ls)
-  (if (or (null? (cdr ls))
-          (not (list? (cdr ls))))
-      ls
-      (my-last (cdr ls))))
-
-(my-last '(a b c d))
-;; (d)
-(my-last '())
-;; error
-(my-last '(a . b))
-;; (a . b)
-
-
-;; answer
-http://www.ic.unicamp.br/~meidanis/courses/mc336/2006s2/funcional/L-99_Ninety-Nine_Lisp_Problems.html
-
-
-
-;; yet another
-(define (my-last ls)
-  (pair-fold (lambda (pr acc)
-               (if (= (length pr) 1)
-                   pr
-                   acc))
-             #f ls))
-
-
-(my-last '(a b c d))
-;; (d)
-(my-last '())
-;; #f
-(my-last '(a . b))
-;; error
-
-
-;; syntax-rules
-(define-syntax my-last
-  (syntax-rules ()
-    ((_ (x))
-     '(x))
-    ((_ (x y))
-     (my-last (y)))
-    ((_ (x y z ...))
-     (my-last (y z ...)))))
-
-(my-last (a b c d))
-;; (d)
-(my-last ())
-;; error
-(my-last (a . b))
-;; error
-
-
-;; failed option
-(define (my-last ls . opt)
-  (let-optionals* opt ((failed #f))
-    (pair-fold (lambda (pr acc)
-                 (if (null? (cdr pr))
-                     pr
-                     acc))
-               failed ls)))
-
-(my-last '(a b c d))
-;; (d)
-(my-last '())
-;; #f
-(my-last '() "hoge")
-;; "hoge"
-(my-last '(a . b))
-;; error
-
-
-;; latest
-(define (my-last ls . opt)
-  (let-optionals* opt ((failed #f))
-    (let/cc hop
-      (pair-fold-right (lambda (pr acc)
-                         (print pr)
-                         (if (null? (cdr pr))
-                             (hop pr)
-                             acc))
-                       failed ls))))
-
-(my-last '(a b c d))
-;; (d)
-(my-last '())
-;; #f
-(my-last '() "hoge")
-;; "hoge"
-(my-last '(a . b))
-;; error
+(use gauche.test)
+(test-start test-name)
+(test* "" '() (my-last '()))
+(test* "" '(d) (my-last '(d)))
+(test* "" '(d) (my-last '(a b c d)))
