033c34f6b581daccc7ee76ef3c5533af213a89d4
valvallow <valvalloooooooooow@gmail.com>
2013-06-29 00:29:29 +0900

    add comment

--- L10.scm
+++ L10.scm
@@ -5,6 +5,7 @@
 ;; * (encode '(a a a a b c c a a d e e e e))
 ;; ((4 A) (1 B) (2 C) (2 A) (1 D)(4 E))
 
+;; L09
 (define (pack ls :optional (cmpfn eq?))
   (and (not (null? ls))
        (let rec ((ls ls)(set '())(acc '()))
