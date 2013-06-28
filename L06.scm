0ca9c1302f33e3f1bac77fdd1c3120ca9172297e
valvallow <valvalloooooooooow@gmail.com>
2013-06-28 22:20:14 +0900

    update L06

--- L06.scm
+++ L06.scm
@@ -1,17 +1,11 @@
-;; L-99
-;; http://github.com/valvallow/L-99
-
 ;; P06 (*) Find out whether a list is a palindrome.
 ;; A palindrome can be read forward or backward; e.g. (x a m a x).
 
 (define (palindrome-list? ls)
   (equal? ls (reverse ls)))
 
-(palindrome-list? '(a b c b a))
-;; #t
-(palindrome-list? '(a b c d e))
-;; #f
-(palindrome-list? '(a b c a b))
-;; #f
+(use gauche.test)
+(test* "" #t (palindrome-list? '(a b c b a)))
+(test* "" #f (palindrome-list? '(a b c d e)))
+(test* "" #f (palindrome-list? '(a b c a b)))
 
-;;
