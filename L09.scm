e0531af4d3af17c62e28458d582ad3225a2d2179
valvallow <valvalloooooooooow@gmail.com>
2013-06-29 00:20:22 +0900

    update L09

--- L09.scm
+++ L09.scm
@@ -1,6 +1,3 @@
-;; L-99
-;; http://github.com/valvallow/L-99
-
 ;; P09 (**) Pack consecutive duplicates of list elements into sublists.
 ;; If a list contains repeated elements they should be placed in separate sublists.
 
@@ -8,143 +5,16 @@
 ;; * (pack '(a a a a b c c a a d e e e e))
 ;; ((A A A A) (B) (C C) (A A) (D) (E E E E))
 
-
-;; ver 1 : named-let
-(define (pack ls)
-  (reverse
-   (let loop ((ls ls)
-              (acc '())
-              (tac '()))
-     (cond ((null? ls) (if (null? tac)
-                           acc
-                           (cons tac acc)))
-           ((null? tac)
-            (loop (cdr ls) acc (cons (car ls) tac)))
-           ((equal? (car ls)(car tac))
-            (loop (cdr ls) acc (cons (car ls) tac)))
-           (else (loop (cdr ls)(cons tac acc)(cons (car ls) '())))))))
-
-(pack '(a a a a b c c a a d e e e e))
-;; ((a a a a) (b) (c c) (a a) (d) (e e e e))
-(pack '(1))
-;; ((1))
-(pack '(1 2 3 1 2 3))
-;; ((1) (2) (3) (1) (2) (3))
-(pack '())
-;; ()
+(define (pack ls :optional (cmpfn eq?))
+  (and (not (null? ls))
+       (let rec ((ls ls)(set '())(acc '()))
+         (cond ((null? ls)(reverse (cons set acc)))
+               ((or (null? set)(cmpfn (car ls)(car set)))
+                (rec (cdr ls)(cons (car ls) set) acc))
+               (else (rec (cdr ls)(cons (car ls) '()) (cons set acc)))))))
 
 
-;; ver 2
-(define (pack ls)
-  (reverse
-   (let loop ((ls ls)
-              (acc '())
-              (tac '()))
-     (cond ((null? ls) (if (null? tac)
-                           acc
-                           (cons tac acc)))
-           ((or (null? tac)
-                (equal? (car ls)(car tac)))
-            (loop (cdr ls) acc (cons (car ls) tac)))
-           (else (loop (cdr ls)(cons tac acc)(cons (car ls) '())))))))
-
-(pack '(a a a a b c c a a d e e e e))
-;; ((a a a a) (b) (c c) (a a) (d) (e e e e))
-(pack '(1))
-;; ((1))
-(pack '(1 2 3 1 2 3))
-;; ((1) (2) (3) (1) (2) (3))
-(pack '())
-;; ()
-
-
-;; ver 3 : pair-fold
-(use srfi-1)
-(define (pack ls)
-  (pair-fold-right
-   (lambda (pr acc)
-     (if (null? acc)
-         (acons (car pr) '() acc)
-         (if (equal? (car pr)(caar acc))
-             (acons (car pr)(car acc)(cdr acc))
-             (acons (car pr) '() acc))))
-   '() ls))
-
-(pack '(a a a a b c c a a d e e e e))
-;; ((a a a a) (b) (c c) (a a) (d) (e e e e))
-(pack '(1))
-;; ((1))
-(pack '(1 2 3 1 2 3))
-;; ((1) (2) (3) (1) (2) (3))
-(pack '())
-;; ()
-
-
-;; ver 4
-(define (pack ls)
-  (pair-fold-right
-   (lambda (pr acc)
-     (if (or (null? acc)
-             (not (equal? (car pr)(caar acc))))
-         (acons (car pr) '() acc)
-         (acons (car pr)(car acc)(cdr acc))))
-   '() ls))
-
-(pack '(a a a a b c c a a d e e e e))
-;; ((a a a a) (b) (c c) (a a) (d) (e e e e))
-(pack '(1))
-;; ((1))
-(pack '(1 2 3 1 2 3))
-;; ((1) (2) (3) (1) (2) (3))
-(pack '())
-;; ()
-
-
-;; ver 5
-(define (pack ls)
-  (pair-fold-right
-   (lambda (pr acc)
-     (apply acons (car pr)
-            (if (or (null? acc)
-                    (not (equal? (car pr)(caar acc))))
-                `(() ,acc)
-                `(,(car acc),(cdr acc)))))
-   '() ls))
-
-(pack '(a a a a b c c a a d e e e e))
-;; ((a a a a) (b) (c c) (a a) (d) (e e e e))
-(pack '(1))
-;; ((1))
-(pack '(1 2 3 1 2 3))
-;; ((1) (2) (3) (1) (2) (3))
-(pack '())
-;; ()
-
-
-;; ver 6
-(define (pack ls . opt)
-  (let-optionals* opt ((eq? eq?))
-    (pair-fold-right
-     (lambda (pr acc)
-       (apply acons (car pr)
-              (if (or (null? acc)
-                      (not (eq? (car pr)(caar acc))))
-                  `(() ,acc)
-                  `(,(car acc),(cdr acc)))))
-     '() ls)))
-
-(pack '(a a a a b c c a a d e e e e))
-;; ((a a a a) (b) (c c) (a a) (d) (e e e e))
-(pack '(1))
-;; ((1))
-(pack '(1 2 3 1 2 3))
-;; ((1) (2) (3) (1) (2) (3))
-(pack '())
-;; ()
-(pack (map (cut cons <> '())
-           '(a a a a b c c a a d e e e e)))
-;; (((a)) ((a)) ((a)) ((a)) ((b)) ((c)) ((c)) ((a)) ((a)) ((d)) ((e)) ((e)) ((e)) ((e)))
-(pack (map (cut cons <> '())
-           '(a a a a b c c a a d e e e e)) equal?)
-;; (((a) (a) (a) (a)) ((b)) ((c) (c)) ((a) (a)) ((d)) ((e) (e) (e) (e)))
+(use gauche.test)
+(test* "" #f (pack '()))
+(test* "" '((a a a a) (b) (c c) (a a) (d) (e e e e)) (pack '(a a a a b c c a a d e e e e)))
 
