fc40b56d0847493b10e757c3b142db7a0a5869c0
valvallow <valvalloooooooooow@gmail.com>
2013-06-28 22:19:42 +0900

    update L05

--- L05.scm
+++ L05.scm
@@ -1,40 +1,14 @@
-;; L-99
-;; http://github.com/valvallow/L-99
-
 ;; P05 (*) Reverse a list.
 
-
-;; letrec
 (define (my-reverse ls)
-  (letrec
-      ((recur (lambda (ls acc)
-                (if (null? ls)
-                    acc
-                    (recur (cdr ls)(cons (car ls) acc))))))
-    (recur ls '())))
-
-(my-reverse '(1 2 3))
-;; (3 2 1)
-
-
-;; named-let
-(define (my-reverse ls)
-  (let loop ((ls ls)(acc '()))
+  (let rec ((ls ls)(acc '()))
     (if (null? ls)
         acc
-        (loop (cdr ls)(cons (car ls) acc)))))
+        (rec (cdr ls)(cons (car ls) acc)))))
 
-(my-reverse '(1 2 3))
-;; (3 2 1)
-
-
-;; fold
 (define (my-reverse ls)
   (fold cons '() ls))
 
-(my-reverse '(1 2 3))
-;; (3 2 1)
-
-
-
-;; 
+(use gauche.test)
+(test* "" '(4 3 2 1) (my-reverse '(1 2 3 4)))
+(test* "" '(d c b a) (my-reverse '(a b c d)))
