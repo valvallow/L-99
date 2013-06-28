e89a3ec12ac65b4dc6cb33261dbf49d30cd2908b
valvallow <valvalloooooooooow@gmail.com>
2013-06-29 00:43:35 +0900

    update L12

--- L12.scm
+++ L12.scm
@@ -1,6 +1,3 @@
-;; L-99
-;; http://github.com/valvallow/L-99
-
 ;; P12 (**) Decode a run-length encoded list.
 ;; Given a run-length code list generated as specified in problem P11. Construct its uncompressed version.
 
@@ -8,53 +5,13 @@
 ;; (a a a a b c c a a d e e e e)
 
 
-;; ---ver1---------------------------------------------------------
-
-;; sub proc
-(define (spread-run-length-list ls)
-  (let loop ((ls ls)(acc '()))
-    (if (zero? (car ls))
-        acc
-        (loop (list (- (car ls) 1)(cadr ls))
-              (cons (cadr ls) acc)))))
-
-(spread-run-length-list (car '((4 a) b (2 c) (2 a) d (4 e))))
-;; (a a a a)
-
-
 (use srfi-1)
 (define (decode-run-length-encoded-list ls)
-  (fold-right (lambda (e acc)
-                (fold-right (lambda (e a)
-                              (cons e a)) acc e))
-              '()
-              (map (lambda (e)
-                     (if (list? e)
-                         (spread-run-length-list e)
-                         (list e)))
-                   ls)))
-
-(decode-run-length-encoded-list '((4 a) b (2 c) (2 a) d (4 e)))
-
-
-
-;; ---ver2---------------------------------------------------------
-
-;; L07
-(define (flatten tree)
-  (fold (lambda (e acc)
-          (append acc
-                  (if (list? e)
-                      (flatten e)
-                      (list e))))
-        '() tree))
-
-
-(define (decode-run-length-encoded-list ls)
-  (flatten (map (lambda (e)
-                  (if (list? e)
-                      (spread-run-length-list e)
+  (append-map (^e (if (list? e)
+                      (list-tabulate (car e) (^ _ (cadr e)))
                       (list e)))
-                ls)))
+              ls))
 
-(decode-run-length-encoded-list '((4 a) b (2 c) (2 a) d (4 e)))
+(use gauche.test)
+(test* "" '(a a a a b c c a a d e e e e)
+       (decode-run-length-encoded-list '((4 a) b (2 c) (2 a) d (4 e))))
