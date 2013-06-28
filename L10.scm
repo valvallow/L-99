f151be201df11d56e00cd1463061513e5fe05ac6
valvallow <valvalloooooooooow@gmail.com>
2013-06-29 00:27:38 +0900

    update L10

--- L10.scm
+++ L10.scm
@@ -1,6 +1,3 @@
-;; L-99
-;; http://github.com/valvallow/L-99
-
 ;; P10 (*) Run-length encoding of a list.
 ;; Use the result of problem P09 to implement the so-called run-length encoding data compression method. Consecutive duplicates of elements are encoded as lists (N E) where N is the number of duplicates of the element E.
 
@@ -8,28 +5,20 @@
 ;; * (encode '(a a a a b c c a a d e e e e))
 ;; ((4 A) (1 B) (2 C) (2 A) (1 D)(4 E))
 
+(define (pack ls :optional (cmpfn eq?))
+  (and (not (null? ls))
+       (let rec ((ls ls)(set '())(acc '()))
+         (cond ((null? ls)(reverse (cons set acc)))
+               ((or (null? set)(cmpfn (car ls)(car set)))
+                (rec (cdr ls)(cons (car ls) set) acc))
+               (else (rec (cdr ls)(cons (car ls) '()) (cons set acc)))))))
 
-;; load L09.scm
-(use srfi-1)
-(define (pack ls . opt)
-  (let-optionals* opt ((eq? eq?))
-    (pair-fold-right
-     (lambda (pr acc)
-       (apply acons (car pr)
-              (if (or (null? acc)
-                      (not (eq? (car pr)(caar acc))))
-                  `(() ,acc)
-                  `(,(car acc),(cdr acc)))))
-     '() ls)))
-
-
-;; ver 1
 (define (encode ls)
-  (let ((ls (pack ls)))
-    (map (lambda (e)
-           (list (length e)(car e)))
-         ls)))
+  (and-let* ((packed (pack ls))
+             packed
+             ((map (^e (list (length e)(car e))) packed)))))
 
-(encode '(a a a a b c c a a d e e e e))
-
+(use gauche.test)
+(test* ""  #f (encode '()))
+(test* ""  '((4 a) (1 b) (2 c) (2 a) (1 d)(4 e)) (encode '(a a a a b c c a a d e e e e)))
 
