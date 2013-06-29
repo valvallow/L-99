6bcd8f115b2071945a29b11503cc571885051498
valvallow <valvalloooooooooow@gmail.com>
2013-06-30 00:28:24 +0900

    update L22

--- L22.scm
+++ L22.scm
@@ -1,39 +1,13 @@
-;; L-99
-;; http://github.com/valvallow/L-99
-
 ;; P22 (*) Create a list containing all integers within a given range.
 ;; If first argument is smaller than second, produce a list in decreasing order.
 ;; Example:
 ;; * (range 4 9)
 ;; (4 5 6 7 8 9)
 
-(define (range min max)
-  (let rec ((cnt max)(acc '()))
-    (if (< cnt min)
-        acc
-        (rec (- cnt 1)(cons cnt acc)))))
+(define (range start end)
+  (iota (- end (- start 1)) start))
 
-(range 4 9)
-;; (4 5 6 7 8 9)
+(use gauche.test)
+(test* "" '(4 5 6 7 8 9) (range 4 9))
 
-(range 10 10)
-;; (10)
-
-
-
-(use srfi-1)
-
-(define (range min max)
-  (list-tabulate (+ (- max min) 1)
-                 (lambda (n)
-                   (+ n min))))
-
-(range 4 9)
-;; (4 5 6 7 8 9)
-
-(define (range min max)
-  (iota (+ (- max min) 1) min))
-
-(range 4 9)
-;; (4 5 6 7 8 9)
 
