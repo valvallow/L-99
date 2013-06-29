9b09a1801a09696ba5b73972985c657969ba9eb1
valvallow <valvalloooooooooow@gmail.com>
2013-06-30 00:24:04 +0900

    update L21

--- L21.scm
+++ L21.scm
@@ -1,41 +1,18 @@
-;; L-99
-;; http://github.com/valvallow/L-99
-
 ;; P21 (*) Insert an element at a given position into a list.
 ;; Example:
 ;; * (insert-at 'alfa '(a b c d) 2)
 ;; (A ALFA B C D)
 
-(use srfi-1)
-(define (fold/index proc init ls . lss)
-  (let1 idx 0
-    (apply fold (lambda args
-                  (begin0
-                      (apply proc (append args (list idx)))
-                    (set! idx (+ idx 1))))
-           init (cons ls lss))))
+(use gauche.sequence)
+(define (insert-at x ls n)
+  (reverse
+   (fold-with-index (^(i e acc)
+                      (cons e (if (= i (- n 1))
+                                  (cons x acc)
+                                  acc)))
+                    '()
+                    ls)))
 
-(define (insert-at ele ls n)
-  (let1 n (- n 1)
-    (reverse
-     (fold/index (lambda (e acc idx)
-                   (cons e (if (= n idx)
-                               (cons ele acc)
-                               acc))) '() ls))))
+(use gauche.test)
+(test* "" '(a alfa b c d) (insert-at 'alfa '(a b c d) 2))
 
-(insert-at 'alfa '(a b c d) 2)
-;; (a alfa b c d)
-
-
-;; named-let
-(define (insert-at ele ls n)
-  (let rec ((ls ls)(acc '())(idx 1))
-    (if (null? ls)
-        (reverse acc)
-        (rec (cdr ls)(cons (car ls)
-                           (if (= n idx)
-                               (cons ele acc)
-                               acc))(+ idx 1)))))
-
-(insert-at 'alfa '(a b c d) 2)
-;; (a alfa b c d)
