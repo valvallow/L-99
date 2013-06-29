9282fd5e1f911f5a1ca743a431e4ce1994589ea4
valvallow <valvalloooooooooow@gmail.com>
2013-06-29 22:22:51 +0900

    gauche-sequence

--- L09.scm
+++ L09.scm
@@ -5,16 +5,21 @@
 ;; * (pack '(a a a a b c c a a d e e e e))
 ;; ((A A A A) (B) (C C) (A A) (D) (E E E E))
 
+
+;; gauche
+(use gauche.sequence)
+(define pack group-sequence)
+
+;; my-pack
 (define (pack ls :optional (cmpfn eq?))
-  (and (not (null? ls))
-       (let rec ((ls ls)(set '())(acc '()))
-         (cond ((null? ls)(reverse (cons set acc)))
-               ((or (null? set)(cmpfn (car ls)(car set)))
-                (rec (cdr ls)(cons (car ls) set) acc))
-               (else (rec (cdr ls)(cons (car ls) '()) (cons set acc)))))))
+  (let rec ((ls ls)(set '())(acc '()))
+    (cond ((null? ls)(reverse (cons set acc)))
+          ((or (null? set)(cmpfn (car ls)(car set)))
+           (rec (cdr ls)(cons (car ls) set) acc))
+          (else (rec (cdr ls)(cons (car ls) '()) (cons set acc))))))
 
 
 (use gauche.test)
-(test* "" #f (pack '()))
+(test* "" '()(pack '()))
 (test* "" '((a a a a) (b) (c c) (a a) (d) (e e e e)) (pack '(a a a a b c c a a d e e e e)))
 
