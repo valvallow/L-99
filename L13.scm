d4f4f38dae56eda1b305fc1986cb3a2a63a2939b
valvallow <valvalloooooooooow@gmail.com>
2013-06-29 22:24:37 +0900

    update L13

--- L13.scm
+++ L13.scm
@@ -1,6 +1,3 @@
-;; L-99
-;; http://github.com/valvallow/L-99
-
 ;; P13 (**) Run-length encoding of a list (direct solution).
 ;; Implement the so-called run-length encoding data compression method directly. I.e. don't explicitly create the sublists containing the duplicates, as in problem P09, but only count them. As in problem P11, simplify the result list by replacing the singleton lists (1 X) by X.
 
@@ -8,21 +5,21 @@
 ;; * (encode-direct '(a a a a b c c a a d e e e e))
 ;; ((4 A) B (2 C) (2 A) D (4 E))
 
-
 (use srfi-1)
 (use srfi-8)
 
 (define (encode-direct ls)
-  (let loop ((ls (reverse ls))(acc '()))
+  (let rec ((ls (reverse ls))(acc '()))
     (if (null? ls)
         acc
-        (receive (taken dropped)(span (pa$ equal? (car ls)) ls)
-          (if (null? ls)
-              acc
-              (loop dropped (cons (if (null? (cdr taken))
-                                      (car taken)
-                                      (list (length taken)(car taken)))
-                                  acc)))))))
+        (receive (taken dropped)
+            (span (pa$ equal? (car ls)) ls)
+          (rec dropped (cons (if (null? (cdr taken))
+                                 (car taken)
+                                 (list (length taken)(car taken)))
+                             acc))))))
 
-(encode-direct '(a a a a b c c a a d e e e e))
-;; ((4 a) b (2 c) (2 a) d (4 e))
+
+(use gauche.test)
+(test* "" '()(encode-direct '()))
+(test* "" '((4 a) b (2 c) (2 a) d (4 e))(encode-direct '(a a a a b c c a a d e e e e)))
