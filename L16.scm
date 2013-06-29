fce56e06de0d5d49246f8ece112bc127d4645201
valvallow <valvalloooooooooow@gmail.com>
2013-06-29 22:47:03 +0900

    update L16

--- L16.scm
+++ L16.scm
@@ -1,24 +1,35 @@
-;; L-99
-;; http://github.com/valvallow/L-99
-
 ;; P16 (**) Drop every N'th element from a list.
 ;; Example:
 ;; * (drop '(a b c d e f g h i k) 3)
 ;; (A B D E G H K)
 
 
+(use srfi-1)
 (define (drop-every-nth ls n)
-  (let loop ((ls ls)(acc '())(cnt 1))
-    (if (null? ls)
-        (reverse acc)
-        (loop (cdr ls)
-              (if (zero? (remainder cnt n))
-                  acc
-                  (cons (car ls) acc))
-              (+ cnt 1)))))
+  (append-map (^(e delete?)
+                (if delete?
+                    '()
+                    (list e)))
+              ls
+              (list-tabulate (length ls)
+                             (^i (zero? (modulo (+ i 1) n))))))
 
-(drop-every-nth '(a b c d e f g h i k) 3)
-;; (a b d e g h k)
+(define (drop-every-nth ls n)
+  (reverse
+   (fold (^(e delete? acc)
+           (if delete?
+               acc
+               (cons e acc)))
+         '()
+         ls
+         (list-tabulate (length ls)
+                        (^i (zero? (modulo (+ i 1) n)))))))
+
+
+(use gauche.test)
+(test* "" '(a b d e g h k) (drop-every-nth '(a b c d e f g h i k) 3))
+(test* "" '(a c e g i) (drop-every-nth '(a b c d e f g h i k) 2))
+(test* "" '() (drop-every-nth '(a b c d e f g h i k) 1))
 
 
 
